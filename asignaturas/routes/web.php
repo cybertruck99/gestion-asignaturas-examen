42c76683e66b7e439370ce4b440c5bde1c8a5086
cybertruck99 <mago.de.hielo675@gmail.com>
2025-12-05 16:46:52 -0400

    Delete asignaturas directory

--- asignaturas/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AsignaturaController;
-
-Route::get('/asignaturas', [AsignaturaController::class, 'listar']);
-Route::post('/asignaturas/crear', [AsignaturaController::class, 'crear']);
